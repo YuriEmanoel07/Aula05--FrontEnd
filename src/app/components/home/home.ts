8ac96cc1ca79040b79fc2e3678392e88d6499b72
Your Name <your.email@example.com>
2026-09-19 20:51:48 -0300

    TESTANDO FUNÇOES

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -6,4 +6,43 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+
+  
+  nomeProduto:string = "Curso de Angular";
+  anuncio:string = `O ${this.nomeProduto} está em promoção`
+  idProduto:number = 123;
+  precoProduto:number = 2.59;
+  promocao:boolean = true;
+
+
+
+      testeVariaveis(): void{    //TODA FUNÇÃO TEM UM TIPO (ex: void)
+      console.log("Nome do Produto: ",this.nomeProduto)
+      console.log("anuncio: ",this.anuncio);
+      console.log(`ID: ` , this.idProduto);
+      console.log(`Preço:`, this.precoProduto);
+      console.log(`Promoção: `, this.promocao);
+
+      //escopo de variaveis dentro do código
+      var variavel1;
+      var variavel;
+      const constante3 = 3;
+
+      var idade = 10;
+
+      function ImprimeIdade(){
+        var idade = 50;
+        console.log('Minha idade é: ', idade);
+      }
+      ImprimeIdade();
+
+      function ImprimeIdade2(){
+        for (let idade = 30; idade <= 40; idade++){
+          console.log('Idade dentro do for: ', idade);
+        }
+        console.log('Idade fora do for: ',idade);
+      }
+      ImprimeIdade2();
+  }
+}
